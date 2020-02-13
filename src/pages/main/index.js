8002f07838dc634d2eb4d398070b1180c69be353
Luiz Guilherme Muraro <luizmuraro@gmail.com>
2020-02-13 14:48:19 -0300

    Adiciona estilizaçao da pagina main

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,28 +1,36 @@
-import React, { Component } from 'react';
-import api from '../../services/api';
+import React, { Component } from "react";
+import api from "../../services/api";
+import './styles.css';
 
 export default class Main extends Component {
-    state = {
-        products: [],
-    }
+  state = {
+    products: []
+  };
 
-    componentDidMount() {
-        this.loadProducts();
-    }
+  componentDidMount() {
+    this.loadProducts();
+  }
 
-    loadProducts = async () => {
-        const response = await api.get('/products');
-       
-        this.setState({ products: response.data.docs })
-    };
+  loadProducts = async () => {
+    const response = await api.get("/products");
 
-    render() {
-        return(
-            <div className="product-list">
-                {this.state.products.map(product => (
-                    <h2 key={product._id}>{product.title}</h2>
-                ))}
-            </div>
-        ) 
-    }
+    this.setState({ products: response.data.docs });
+  };
+
+  render() {
+    const { products } = this.state;
+
+    return (
+      <div className="product-list">
+        {products.map(product => (
+          <article key={product._id}>
+            <strong>{product.title}</strong>
+            <p>{product.description}</p>
+
+            <a href="">Acessar</a>
+          </article>
+        ))}
+      </div>
+    );
+  }
 }
